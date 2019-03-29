71c48a99283b76fff7383979fbbb14e1c616b4e8
YusupovAI <iusupov.ai@phystech.edu>
2019-03-29 23:35:44 +0300

    Cmake fixed bug in ubuntu

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/artyom/TP
+# Build directory: /home/artyom/TP
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib")
+subdirs("code")
---
+++ code/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/artyom/TP/code
+# Build directory: /home/artyom/TP/code
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Army")
